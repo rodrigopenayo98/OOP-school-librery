8d78fe624ab42dd978c3862950b9264e4300c1e6
rod <rodrip0129@gmail.com>
2023-09-06 11:28:19 -0300

    set up associations

--- main.rb
+++ main.rb
@@ -7,7 +7,11 @@ require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
 
-person = Person.new(age: 20, name: 'John Doe', parent_permission: false)
+require_relative 'classroom'
+require_relative 'book'
+require_relative 'rental'
+
+person = Person.new(name: 'John Doe', age: 20, parent_permission: false)
 puts "Original Name: #{person.correct_name}"
 
 capitalized_person = CapitalizeDecorator.new(person)
@@ -16,11 +20,22 @@ puts "Capitalized Name: #{capitalized_person.correct_name}"
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
 
-student = Student.new('Alice', 16, true, 'Class A')
-puts "Student ID: #{student.id}"
-puts "Can use services? #{student.can_use_services?}"
-puts "Play hooky: #{student.play_hooky}"
+# Crear una instancia de Classroom
+classroom = Classroom.new('Class A')
 
-teacher = Teacher.new(35, 'Mr. Smith', 'Mathematics')
-puts "Teacher ID: #{teacher.id}"
-puts "Can use services? #{teacher.can_use_services?}"
+# Crear una instancia de Book
+book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
+
+# Crear una instancia de Rental para relacionar Person y Book
+Rental.new('2023-09-04', book, person)
+
+# Agregar el estudiante a la aula
+classroom.add_student(person)
+
+# Probar las relaciones y clases
+puts "Classroom Label: #{classroom.label}"
+puts "Classroom Students: #{classroom.students.map(&:name).join(', ')}"
+puts "Book Title: #{book.title}, Author: #{book.author}"
+puts "Book Rentals: #{book.rentals.map(&:date).join(', ')}"
+puts "Person Name: #{person.name}, Age: #{person.age}"
+puts "Person Rentals: #{person.rentals.map(&:date).join(', ')}"
