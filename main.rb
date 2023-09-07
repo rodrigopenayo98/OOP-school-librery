a52f7c4499b7906e75067ff5c6b6b1a38e373f72
rod <rodrip0129@gmail.com>
2023-09-07 00:14:25 -0300

    add basic ui

--- main.rb
+++ main.rb
@@ -10,6 +10,7 @@ require_relative 'teacher'
 require_relative 'classroom'
 require_relative 'book'
 require_relative 'rental'
+require_relative 'app'
 
 person = Person.new(name: 'John Doe', age: 20, parent_permission: false)
 puts "Original Name: #{person.correct_name}"
@@ -20,22 +21,21 @@ puts "Capitalized Name: #{capitalized_person.correct_name}"
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
 
-# Crear una instancia de Classroom
 classroom = Classroom.new('Class A')
 
-# Crear una instancia de Book
 book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
 
-# Crear una instancia de Rental para relacionar Person y Book
 Rental.new('2023-09-04', book, person)
 
-# Agregar el estudiante a la aula
 classroom.add_student(person)
 
-# Probar las relaciones y clases
 puts "Classroom Label: #{classroom.label}"
 puts "Classroom Students: #{classroom.students.map(&:name).join(', ')}"
 puts "Book Title: #{book.title}, Author: #{book.author}"
 puts "Book Rentals: #{book.rentals.map(&:date).join(', ')}"
 puts "Person Name: #{person.name}, Age: #{person.age}"
 puts "Person Rentals: #{person.rentals.map(&:date).join(', ')}"
+
+app = App.new
+
+app.run
