d0e5a5e71daa9f47ec93318fdbc2f1a0023564ae
rod <rodrip0129@gmail.com>
2023-09-11 11:44:41 -0300

    refact main file

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(name, age, parent_permission, classroom = nil)
-    super(age: age, name: name, parent_permission: parent_permission)
+    super(name: name,age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
